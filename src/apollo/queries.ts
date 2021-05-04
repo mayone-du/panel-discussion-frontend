b84ac80b67cc56f9ec39bb680456baeefde1735d
mayone-du <cocomayo1201@icloud.com>
2021-05-04 18:59:04 +0900

    Create get token mutation

--- src/apollo/queries.ts
+++ src/apollo/queries.ts
@@ -1,5 +1,13 @@
 import { gql } from "@apollo/client";
 
+export const GET_AUTH_TOKEN = gql`
+  mutation($username: String!, $password: String!) {
+    tokenAuth(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
 export const GET_ALL_TOPICS = gql`
   query {
     allTopics {
