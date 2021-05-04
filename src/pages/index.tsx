939be9e5ec3be19560ce088215f4263ffb7f49ef
mayone-du <cocomayo1201@icloud.com>
2021-05-04 17:18:05 +0900

    Run Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,44 @@
 import { useQuery } from "@apollo/client";
 import { useEffect } from "react";
 import { GET_ALL_TOPICS } from "src/apollo/queries";
-import {ALL_TOPICS} from 'src/types/types';
+import { ALL_TOPICS } from "src/types/types";
 
 const Index: React.FC = () => {
-  const { loading, error, data }: {loading: any, error: any, data: ALL_TOPICS} = useQuery(GET_ALL_TOPICS);
-
+  const {
+    loading,
+    error,
+    data,
+  }: { loading: any; error: any; data: ALL_TOPICS } = useQuery(GET_ALL_TOPICS);
 
   return (
     <>
       <div>
-        {loading
-          ? <div className="text-6xl">Loading</div>
-          : data.allTopics.edges.map((topic, index) => {
-              return (
-                <div className="border m-8" key={index}>
-                  <div className="text-lg">{topic.node.id}</div>
-                  <div className="text-xl">{topic.node.title}</div>
-                  <div className={topic.node.isTalking ? "text-red-600" : "text-blue-600"}>isTalking: {String(topic.node.isTalking)}</div>
-                  <div className={topic.node.isClosed ? "text-red-600" : "text-blue-600"}>isClosed: {String(topic.node.isClosed)}</div>
+        {loading ? (
+          <div className="text-6xl">Loading</div>
+        ) : (
+          data.allTopics.edges.map((topic, index) => {
+            return (
+              <div className="border m-8" key={index}>
+                <div className="text-lg">{topic.node.id}</div>
+                <div className="text-xl">{topic.node.title}</div>
+                <div
+                  className={
+                    topic.node.isTalking ? "text-red-600" : "text-blue-600"
+                  }
+                >
+                  isTalking: {String(topic.node.isTalking)}
                 </div>
-              );
-            })}
+                <div
+                  className={
+                    topic.node.isClosed ? "text-red-600" : "text-blue-600"
+                  }
+                >
+                  isClosed: {String(topic.node.isClosed)}
+                </div>
+              </div>
+            );
+          })
+        )}
       </div>
     </>
   );
