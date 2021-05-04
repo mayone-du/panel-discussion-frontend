97fd0d63a74a1aa4e91635aa4b4884009fbbdf5f
mayone-du <cocomayo1201@icloud.com>
2021-05-04 10:59:20 +0900

    Setting appolo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import "src/styles/globalResets.css"
-import "src/styles/globals.css"
+import "src/styles/globalResets.css";
+import "src/styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+  const client = new ApolloClient({
+    uri: `${process.env.NEXT_PUBLIC_DEV_API_URL}graphql/`,
+    headers: {
+      authorization: "",
+    },
+    cache: new InMemoryCache(),
+  });
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
-export default App
+export default App;
