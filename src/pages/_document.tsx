9553d388450142bc7d5c604511d54c19dd0931fb
mayone-du <cocomayo1201@icloud.com>
2021-05-06 19:41:57 +0900

    Add _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          <meta
+            property="og:url"
+            content="https://qin-paneldiscussion.vercel.app/"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Qin PanelDicussion" />
+          <meta
+            property="og:description"
+            content="パネルディスカッション用アプリ"
+          />
+          <meta property="og:site_name" content="Qin PanelDiscussion" />
+          <meta property="og:image" content="/images/og-image.png" />
+
+          <meta name="twitter:card" content="Summary Card" />
+          <meta name="twitter:site" content="@mayo1201blog" />
+          <title>Qin PanelDiscussion</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
